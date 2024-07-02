41ea525d4292d5b8ba121b6be392ddd66d769d16
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-07-02 02:26:53 +0100

    feat: static dummy shop products data

---
+++ src/data/products.ts
@@ -0,0 +1,70 @@
+import { Product } from "../components/product-item/interface";
+
+export const products: Product[] = [
+  {
+    id: "1",
+    name: "Men's Short Sleeve T-Shirt",
+    price: 4990,
+    image: require("../../assets/images/t_shirt.jpg"),
+  },
+  {
+    id: "2",
+    name: "Smart STOCK Jeans",
+    price: 12999,
+    image: require("../../assets/images/jeans.jpg"),
+  },
+  {
+    id: "3",
+    name: "Monospec Sneakers - White",
+    price: 18900,
+    image: require("../../assets/images/sneakers.jpg"),
+  },
+  {
+    id: "4",
+    name: "Summer Unisex Reversible Bucket Hat For Men and Women",
+    price: 5500,
+    image: require("../../assets/images/hat.jpg"),
+  },
+  {
+    id: "5",
+    name: "Apple iPhone 15pro 128gb - Nano Sim Natural Titanium",
+    price: 1600000,
+    image: require("../../assets/images/iPhone.jpg"),
+  },
+  {
+    id: "6",
+    name: "Designer Baseball Face Cap",
+    price: 3900,
+    image: require("../../assets/images/cap.jpg"),
+  },
+  {
+    id: "7",
+    name: "Logitech Geniune G102 Gaming mouse",
+    price: 8000,
+    image: require("../../assets/images/mouse.jpg"),
+  },
+  {
+    id: "8",
+    name: "Sony PS4",
+    price: 12000,
+    image: require("../../assets/images/gamepad.jpg"),
+  },
+  {
+    id: "9",
+    name: "2 in 1 Check Pattern Ladies Handbag - Black",
+    price: 8733,
+    image: require("../../assets/images/bag.jpg"),
+  },
+  {
+    id: "10",
+    name: "Mens Comfortable Joggers Pant",
+    price: 9280,
+    image: require("../../assets/images/joggers.jpg"),
+  },
+  {
+    id: "11",
+    name: "Mechanical keyboard and mouse with bright light",
+    price: 12999,
+    image: require("../../assets/images/keyboard.jpg"),
+  },
+];
